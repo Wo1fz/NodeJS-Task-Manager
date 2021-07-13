59882ce88d9522b925dbab1a709e608d15b02ed6
Wo1fz <nmy_97@hotmail.com>
2021-07-13 20:16:47 +0800

    Setting Up .env file

--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose')
 
-const connectionString =
-  'mongodb+srv://wo1fz:123Qweasd@wo1fz-nodejs.quldp.mongodb.net/nodeJS-Task-Manager?retryWrites=true&w=majority'
-
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('connected to the db...'))
-  .catch((err) => console.log(err))
+}
+
+module.exports = connectDB
